5209581e306a3bcc3271902718c83bd024cfe1e9
Thanachok Suwan <119285237+thanachok11@users.noreply.github.com>
2025-07-07 17:57:57 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use('/api', vehicleRoutes);  // เพิ่มตรงนี้
+app.use('/api', vehicleRoutes);
 
 app.get('/', (req, res) => {
     res.send('API is running...');
