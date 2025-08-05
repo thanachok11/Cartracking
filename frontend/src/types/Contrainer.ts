6ec4864e52699b0668192fe59de17cff32d0645a
Thanachok Suwan <119285237+thanachok11@users.noreply.github.com>
2025-08-05 15:05:03 +0700

    Delete frontend directory

--- frontend/src/types/Contrainer.ts
+++
@@ -1,9 +0,0 @@
-export interface Contrainers {
-    _id: string;
-    containerNumber: string;
-    companyName: string;
-    containerSize: string;
-    createdBy?: string;
-    createdAt?: string;
-    updatedAt?: string;
-}
